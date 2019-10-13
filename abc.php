f1c81c0ad1a34a2f4f050ff54015814bdcef6268
Nazib Mahfuz <41067328+MahfuzNazib@users.noreply.github.com>
2019-10-13 10:17:23 +0600

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,14 @@
+<?php
+	$username=$_POST['uname'];
+	$password=$_POST['password'];
+	$dob=$_POST['dob'];
+	$address=$_POST['address'];
+	$dept=$_POST['dept'];
+	$gender=$_POST['gender'];
+	$chk=$_POST['']
+	for($i=0;$i<3,$i++)
+	{
+		$i;
+	}
+	echo $username." ".$password." ".$dob." ".$address." ".$dept." ".$gender." ".$i;
+?>
